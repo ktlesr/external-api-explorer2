68b88aa3347798eacb6b2e8d38caf1ffd354d78d
ktlesr <134289884+ktlesr@users.noreply.github.com>
2025-11-29 20:59:44 +0300

    Add hardcoded Supabase URL and key
    Hardcode Supabase URL and key in createClient function.

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,6 +1,9 @@
 import { createBrowserClient } from "@supabase/ssr"
 
 export function createClient(url?: string, key?: string) {
+  const url = "https://zyxiznikuvpwmopraauj.supabase.co";
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5eGl6bmlrdXZwd21vcHJhYXVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4NDU5ODQsImV4cCI6MjA2NDQyMTk4NH0.YNf5WA5grzswrRKl5SfiZh1dZM9esA66vvHI5fATPm8";
+
   const supabaseUrl = url || process.env.NEXT_PUBLIC_SUPABASE_URL
   const supabaseKey = key || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
